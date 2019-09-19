1289bd0af0c62a75cff1892ea230e18990cc8a5f
whtang906 <whtang906@gmail.com>
2019-09-20 02:28:36 +0800

    complete all basic functions

--- background.js
+++ background.js
@@ -1,34 +1,43 @@
-const BASE_URL = 'https://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/';
+const BASE_URL = "https://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/";
 const SEARCH_URL = `${BASE_URL}search.php?word=`;
 
-function getPronunciation(word) {
-        fetch(`${SEARCH_URL}${word}`, {
-            method: "GET",
-            mode: "cors"
-        })
-            .then(res => res.text())
-            .then(text => {
-                let parser = new DOMParser();
-                let html = parser.parseFromString(text, "text/html");
-                let audioPlayerEl = html.querySelector('#char_can_table .char_can_head>a');
-                if (audioPlayerEl) {
-                    const audioUrl = audioPlayerEl.getAttribute('onclick').match(/'([^']+)'/)[1];
-                    console.log(`${BASE_URL}${audioUrl}`);
-                    let audio = new Audio(`${BASE_URL}${audioUrl}`);
-                    audio.play();
-                }
-            });
+function getPronunciations(word) {
+    let pronunciations = [];
+    return fetch(`${SEARCH_URL}${word}`, {
+        method: "GET",
+        mode: "cors"
+    })
+        .then(res => res.text())
+        .then(text => {
+            let parser = new DOMParser();
+            let html = parser.parseFromString(text, "text/html");
+            let audioPlayerEl = html.querySelectorAll("#char_can_table .char_can_head>a");
+
+            if (audioPlayerEl) {
+                audioPlayerEl.forEach(pronunciationEl => {
+                    const audioUrl = pronunciationEl.getAttribute("onclick").match(/'([^']+)'/)[1];
+                    const pronunciation = audioUrl.match(/sound\/([^']+).[mM][pP]3/)[1];
+
+                    pronunciations.push({
+                        pronunciation,
+                        audioUrl: `${BASE_URL}${audioUrl}`
+                    });
+                });
+            }
+            return pronunciations;
+        });
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request)
-    switch(request.type) {
-        case 'getPronunciation':
-            getPronunciation(request.word);
+    switch (request.type) {
+        case "getPronunciations":
+            getPronunciations(request.word)
+                .then(result => sendResponse({ pronunciations: result }))
             break;
         default:
     }
-})
 
-chrome.runtime.onInstalled.addListener(function() {
+    return true;
 });
+
+chrome.runtime.onInstalled.addListener(function() {});
--- contentscript.js
+++ contentscript.js
@@ -1,14 +1,91 @@
-document.addEventListener("mouseup", function(event) {
-    let selectedString = window.getSelection().toString();
-    let re = /^[\u4E00-\u9FA5]+$/;
+function playAudio(url) {
+    let audio = new Audio(url);
+    audio.play();
+};
 
-    if (selectedString && selectedString.length === 1 && re.test(selectedString)) {
-        console.log(event.clientX, event.clientY);
-        // TODO - Insert button near the string position
+function adjustPopoverPosition(selectedStringPosition, popover) {
+    const OFFSET = 12;
+    const popoverHeight = popover.offsetHeight;
+    const popoverWidth = popover.offsetWidth;
+    const { top: stringTop, right: stringRight, bottom: stringBottom, left: stringLeft } = selectedStringPosition;
 
-        chrome.runtime.sendMessage({
-            type: 'getPronunciation',
-            word: selectedString
-        });
+    let x, y;
+    x = window.scrollX + (stringLeft + stringRight - popoverWidth) / 2;
+    
+    if (popoverHeight < window.scrollY + stringTop - OFFSET) {
+        y = window.scrollY + stringTop - popoverHeight - OFFSET;
+    } else {
+        y = stringBottom + OFFSET;
+        popover.querySelector('#cp-popover-arrow').className += 'upside-down';
+        popover.querySelector('#cp-popover-arrow-outer').className += 'upside-down';
+    }
+
+    popover.setAttribute('style', `top: ${y}px; left: ${x}px;`);
+    popover.style.visibility = 'visible';
+};
+
+function closePopover() {
+    let popoverEl = document.getElementById('cp-popover');
+    if (popoverEl) {
+        popoverEl.remove();
+    }
+}
+
+window.addEventListener('mouseup', function(e) {
+    // Remove existing popover if popover outside is clicked
+    let popoverEl = document.getElementById('cp-popover');
+    if (popoverEl) {
+        if (!popoverEl.contains(e.target)) {
+            popoverEl.remove();
+        }
+    }
+});
+
+document.addEventListener('mouseup', function(e) {
+    let selectedEl = window.getSelection();
+    if (selectedEl.rangeCount) {
+        let selectedStringPosition = selectedEl.getRangeAt(0).getBoundingClientRect();
+        let selectedString = selectedEl.toString();
+        let re = /^[\u4E00-\u9FA5]+$/;
+    
+        if (selectedString && selectedString.length === 1 && re.test(selectedString)) {
+            // TODO - Insert button near the string position
+            fetch(chrome.extension.getURL('/popover.html'))
+                .then(res => res.text())
+                .then(text => {
+                    let parser = new DOMParser();
+                    let doc = parser.parseFromString(text, 'text/html');
+                    let popover = doc.querySelector('#cp-popover');
+                    let closeBtn = popover.querySelector('#cp-popover-close-btn');
+                    let content = popover.querySelector('#cp-popover-content');
+                    let title = content.querySelector('#cp-popover-content-title');
+                    let pronunciationList = content.querySelector('#cp-popover-content-pronunciation-list');
+                    let creditLink = content.querySelector('#cp-popover-credit>a');
+
+                    closeBtn.onclick = (e) => closePopover();
+                    title.innerHTML = selectedString;
+                    creditLink.href = `https://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/search.php?word=${selectedString}`
+
+                    document.body.appendChild(popover);
+
+                    chrome.runtime.sendMessage({
+                        type: 'getPronunciations',
+                        word: selectedString
+                    }, function(result) {
+                        result.pronunciations.forEach((pronunciation, index) => {
+                            let pronunciationEl = document.createElement('div');
+
+                            pronunciationEl.className = 'cp-popover-content-pronunciation';
+                            pronunciationEl.innerHTML = `${index + 1}. ${pronunciation.pronunciation}`;
+                            pronunciationEl.onclick = (e) => playAudio(pronunciation.audioUrl);
+
+                            pronunciationList.appendChild(pronunciationEl);
+                        })
+                        
+                        adjustPopoverPosition(selectedStringPosition, popover);
+                    });
+                })
+    
+        }
     }
 });
